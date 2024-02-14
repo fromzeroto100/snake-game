63e0d17d34749442431c820368e4b1f22a364cc5
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-02-14 21:29:49 +0100

    new4

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake game")
-turtle = Turtle()
+
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
 
-all_turtles = []
 
-for turtle_index in range(0,3):
-    new_turtle = Turtle(shape="square")
-    new_turtle.color("white")
-    new_turtle.position(x=0, y=20 + 20)
+for position in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)
 
 
 
