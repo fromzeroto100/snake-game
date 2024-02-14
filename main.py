6242492039b45fc5cb421595b4b191fa7551ed20
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-02-14 20:09:07 +0100

    new2

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake game")
+turtle = Turtle()
+
+
+all_turtles = []
+
+for turtle_index in range(0,3):
+    new_turtle = Turtle(shape="square")
+    new_turtle.color("white")
+    new_turtle.position(x=0, y=20 + 20)
+
+
 
 
 
