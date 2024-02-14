e6bfa029f157274ed21142bd95b595e8824fc6c5
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-02-14 19:47:18 +0100

    new1

---
+++ main.py
@@ -0,0 +1,38 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake game")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
