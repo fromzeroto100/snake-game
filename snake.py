d31ad6a6dc7178ba9de6af2f4b4225fbb91c1eca
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-02-15 20:50:19 +0100

    NEW6

--- snake.py
+++ snake.py
@@ -17,13 +17,13 @@ class Snake:
 
 
 
-def move(self):
-    for seg_num in range(len(self.segments) - 1, 0, -1):
-        new_x = self.segments[seg_num - 1].xcor()
-        new_y = self.segments[seg_num - 1].ycor()
-        self.segments[seg_num].goto(new_x, new_y)
-        
-    self.segments[0].forward(MOVE_DISTANCE)    
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+            
+        self.segments[0].forward(MOVE_DISTANCE)    
 
 
 
