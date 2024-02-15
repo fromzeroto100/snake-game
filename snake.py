f2c3f43d794d98b982717dc518e3441e8d12f7d7
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-02-15 20:23:07 +0100

    new2

---
+++ snake.py
@@ -0,0 +1,63 @@
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+
+    def create_snake(self): 
+        for position in starting_positions:
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup( )
+        new_segment.goto(position)
+        segments.append(new_segment) 
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake game")
+screen.tracer(0)
+
+
+segments = []
+
+
+
+
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+        
+    segments[0].forward(20)    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick() 
