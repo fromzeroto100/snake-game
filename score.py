44d3876fa8ccae484058927d4a7f3d1d233a24f4
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-02-18 20:09:53 +0100

    new1

--- score.py
+++ score.py
@@ -8,6 +8,16 @@ class Scoreboard(Turtle):
         self.color("white")
         self.penup()
         self.goto(0, 270)
-        self.write(f"Score: {self.score}", align="center", font="Arial, 24, Normal")
-        self.hideturtle()
         
+        self.hideturtle()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align="center", font="Arial, 24, Normal")
+
+    def game_over(self):
+        self.write(f"GAME OVER", align="center", font="Arial, 24, Normal")
+    
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
